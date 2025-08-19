2a97db9e3f94e9c9f690ce1d81b473df83c32d68
Savitha M <savitha@Savithas-MacBook-Air.local>
2025-08-19 16:09:22 +0530

    gfg setup and build chnages imp

--- target/apidocs/script-files/search.js
+++ target/apidocs/script-files/search.js
@@ -17,7 +17,7 @@ const messages = {
 const categories = {
     modules: "Modules",
     packages: "Packages",
-    types: "Classes and Interfaces",
+    types: "Types",
     members: "Members",
     searchTags: "Search Tags"
 };
