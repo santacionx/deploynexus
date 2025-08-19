908d099e51e32f30a8c990290933fb0fc9baa6c1
Savitha M <savitha@Savithas-MacBook-Air.local>
2025-08-19 15:59:47 +0530

    gfg setup and build chnages

--- src/main/java/com/github/santacionx/deploynexus/App.java
+++ src/main/java/com/github/santacionx/deploynexus/App.java
@@ -1,7 +1,17 @@
 package com.github.santacionx.deploynexus;
 
+/**
+ * A simple application class.
+ */
 public class App {
+
+    /**
+     * Returns a greeting message for the given name.
+     *
+     * @param name the name to greet
+     * @return greeting message
+     */
     public static String sayHello(String name) {
-        return "Hello, " + name + "!";
+        return "Hello, " + name;
     }
 }
